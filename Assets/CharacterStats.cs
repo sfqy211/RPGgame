7e9255ccca88c2dd69f33a9f764afc12ba7c5d1b
coldwind886 <hangruizhang63@gmail.com>
2024-12-12 12:31:47 +0800

    添加魔法属性伤害

--- Assets/CharacterStats.cs
+++ Assets/CharacterStats.cs
@@ -2,24 +2,29 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    [Header("Major stats 主要数值")]
-    public Stat strength; // 力量：增加基础伤害和暴击伤害
+    [Header("Major stats 主要数值")] public Stat strength; // 力量：增加基础伤害和暴击伤害
     public Stat agility; // 敏捷：增加闪避率和暴击率
     public Stat intelligence; // 智慧：增加魔法伤害和魔法抗性
     public Stat vitality; // 活力：增加最大生命值
 
-    [Header("Offensive stats 攻击数值")] 
-    public Stat damage;
+    [Header("Offensive stats 攻击数值")] public Stat damage;
     public Stat critChance; // 暴击率
     public Stat critPower; // 暴击倍率
-    
-    [Header("Defensive stats 防御数值")]
-    public Stat maxHealth;
+
+    [Header("Defensive stats 防御数值")] public Stat maxHealth;
     public Stat armor; // 防御力：减少伤害
     public Stat evasion; // 闪避值（灵敏度）
-    
+    public Stat magicResistance;
 
-    [SerializeField]private int currentHealth;
+    [Header("Magic stats 魔法伤害")] public Stat fireDamage; // 火元素伤害
+    public Stat iceDamage; // 冰元素伤害
+    public Stat lightningDamage; // 电元素伤害
+
+    public bool isIgnited; // 是否被点燃
+    public bool isChilled; // 是否被冰冻
+    public bool isShocked; // 是否被电击
+
+    [SerializeField] private int currentHealth;
 
     protected virtual void Start()
     {
@@ -31,33 +36,66 @@ public class CharacterStats : MonoBehaviour
     {
         if (TargetCanAvoidAttack(_targetStats))
             return;
-        
+
         int totalDamage = damage.GetValue() + strength.GetValue();
-        
-        if(CanCrit())
+
+        if (CanCrit())
         {
             totalDamage = CalculateCriticalDamage(totalDamage);
         }
 
         totalDamage = CheckTargetArmor(_targetStats, totalDamage);
-        _targetStats.TakeDamage(totalDamage);
+        // _targetStats.TakeDamage(totalDamage);
+        DoMagicalDamage(_targetStats);
     }
-    
+
+    public virtual void DoMagicalDamage(CharacterStats _targetStats)
+    {
+        int _fireDamage = fireDamage.GetValue();
+        int _iceDamage = iceDamage.GetValue();
+        int _lightningDamage = lightningDamage.GetValue();
+        
+        int totalMagicDamage = _fireDamage + _iceDamage + _lightningDamage + intelligence.GetValue();
+        
+        totalMagicDamage = CheckTargetResistance(_targetStats, totalMagicDamage);
+        _targetStats.TakeDamage(totalMagicDamage);
+    }
+
+    private static int CheckTargetResistance(CharacterStats _targetStats, int totalMagicDamage)
+    {
+        totalMagicDamage -= _targetStats.magicResistance.GetValue() + (_targetStats.intelligence.GetValue() * 3);
+        totalMagicDamage = Mathf.Clamp(totalMagicDamage, 0, int.MaxValue);
+        return totalMagicDamage;
+    }
+
+    public void ApplyAilments(bool _ignite, bool _chill, bool _shock)
+    {
+        if (isIgnited || isChilled || isShocked)
+            return;
+        // 保证只有一个状态存在
+
+        isIgnited = _ignite;
+        isChilled = _chill;
+        isShocked = _shock;
+    }
+
     public virtual void TakeDamage(int _damage)
     {
         currentHealth -= _damage;
 
         Debug.Log(_damage);
-        
+
         if (currentHealth < 0)
         {
             Die();
         }
     }
+
     protected virtual void Die()
     {
         // throw new NotImplementedException();
     }
+
     private bool TargetCanAvoidAttack(CharacterStats _targetStats)
     {
         int totalEvasion = _targetStats.evasion.GetValue() + agility.GetValue();
@@ -66,21 +104,25 @@ public class CharacterStats : MonoBehaviour
         {
             return true;
         }
+
         return false;
     }
+
     private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
     {
         totalDamage -= _targetStats.armor.GetValue();
         totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
         return totalDamage;
     }
+
     private bool CanCrit()
     {
         int totalCriticalChance = critChance.GetValue() + agility.GetValue();
-        if(Random.Range(0, 100) <= totalCriticalChance)
+        if (Random.Range(0, 100) <= totalCriticalChance)
             return true;
         return false;
     }
+
     private int CalculateCriticalDamage(int _damage)
     {
         float totalCritPower = (critPower.GetValue() + strength.GetValue()) * .01f;
