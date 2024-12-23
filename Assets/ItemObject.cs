b8d42570ab18f6c6f9824fa012f58312bf211025
coldwind886 <hangruizhang63@gmail.com>
2024-12-23 21:02:41 +0800

    开始构建物品系统

---
+++ Assets/ItemObject.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    [SerializeField] private ItemData itemData;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = itemData.icon;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() != null)
+        {
+            Inventory.instance.AddItem(itemData);
+            Destroy(gameObject);
+        }
+    }
+}
